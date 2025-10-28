4d06fd23be81edc9ac63432e43611f764e678c90
Soorajcp <168804842+Soorajcp@users.noreply.github.com>
2025-10-28 12:55:24 +0530

    updated index.js

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ app.use(express.json());  // position chnaged
 
 app.use(
     cors({
-        origin: ["http://localhost:5173" , "https://car-rental-client-topaz.vercel.app"],
+        origin: "http://localhost:5173",
         credentials: true, 
         methods: ["GET", "POST", "PUT", "DELETE"] 
     })
