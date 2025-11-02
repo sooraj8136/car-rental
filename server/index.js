ed9289f1555c4bab05cfc50119406deb2f32e22d
Soorajcp <168804842+Soorajcp@users.noreply.github.com>
2025-11-02 22:21:45 +0530

    Index file changed

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ app.use(express.json());  // position chnaged
 
 app.use(
     cors({
-        origin: ["http://localhost:5173"],
+        origin: ["http://localhost:5173", "https://car-rental-client-lyart.vercel.app"],
         credentials: true,
         methods: ["GET", "POST", "PUT", "DELETE"]
     })
