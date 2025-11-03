26aa9fb4047d83b6ee7d29cd7df5dca7fc945d2b
Soorajcp <168804842+Soorajcp@users.noreply.github.com>
2025-11-03 12:53:22 +0530

    index file value chnaged

--- server/index.js
+++ server/index.js
@@ -92,8 +92,8 @@ const express = require("express");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
 const dotenv = require("dotenv");
-const connectDB = require("../config/db");
-const apiRouter = require("../routes");
+const connectDB = require("./config/db");
+const apiRouter = require("./routes");
 
 dotenv.config(); // only once!
 
